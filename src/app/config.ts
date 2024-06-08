266248903f0300c7e5b22ed39c16f092e4e13869
Alejandro Sánchez Fernández <alexius1996@gmail.com>
2024-06-08 18:38:36 +0200

    asdas

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 // export const backAPIUrl = 'http://localhost:80/api';
 // export const gamesApiUrl = 'http://localhost:81/api';
 
-export const backAPIUrl = 'https://meetoplay.duckdns.org/api';
+export const backAPIUrl = 'https://meetoplay-backend.duckdns.org/api';
 export const gamesApiUrl = 'https://meetoplay-externa.duckdns.org/api';
 
 
