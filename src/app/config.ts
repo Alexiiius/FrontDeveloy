baff4ef26ce072b9a4369cfb60bc9d95c7d451c1
Daniel Ramírez Vaquero <dramvaq@gmail.com>
2024-06-11 09:37:03 +0200

    global chats added

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,18 +1,14 @@
 // export const backAPIUrl = 'http://localhost:80/api';
 // export const gamesApiUrl = 'http://localhost:81/api';
 
-//  35.173.106.192
-//  sudo cat /var/log/apache2/error.log
-
-//  sudo cat /var/log/websocket.err.log
-//  sudo cat /var/log/websocket.out.log
-
-// cat /var/log/laravel_queue.out.log
-// cat /var/log/laravel_queue.err.log
-
-// sudo cat /var/www/html/meetoplay/MeetoPlay/Backend/storage/logs/laravel.log
 
 export const backAPIUrl = 'https://meetoplay.duckdns.org/api';
 export const gamesApiUrl = 'https://meetoplay-externa.duckdns.org/api';
+
+// export const backAPIUrl = 'http://35.173.106.192:80/api';
+// export const gamesApiUrl = 'http://3.93.159.150:80/api';
+
+// export const backAPIUrl = 'https://meetoplay.duckdns.org/api';
+// export const gamesApiUrl = 'https://meetoplay-externa.duckdns.org/api';
 
 
--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -18,12 +18,17 @@ export class WebSocketService {
   private Echo: Echo;
   private Pusher: typeof Pusher;
 
+  private publicMessageSource = new Subject<SocketMessage>();
+  publicMessage$ = this.publicMessageSource.asObservable();
+
   private privateMessageSource = new Subject<SocketMessage>();
   privateMessage$ = this.privateMessageSource.asObservable();
 
   private privateMessagesChannel: string;
 
+  private publicDone: boolean = false;
   private privateDone: boolean = false;
+
 
   constructor() {
     this.Pusher = Pusher;
@@ -31,6 +36,7 @@ export class WebSocketService {
 
 
   setupEchoPublic() {
+    if (this.publicDone) return;
 
     this.Echo = new Echo({
       broadcaster: 'reverb',
@@ -38,46 +44,49 @@ export class WebSocketService {
       wsHost: 'meetoplay.duckdns.org',
       wsPort: 8085,
       wssPort: 443,
-      forceTLS: false,
+      forceTLS: true,
       enabledTransports: ['ws', 'wss'],
     });
 
-    this.Echo.channel(`public`).listen('GlobalMessage', (e: any) => {
+    this.Echo.channel(`public`).listen('GlobalMessage', (response: any) => {
       console.log("From public channel");
-      console.log(e);
+      console.log(response);
+
+      this.publicMessageSource.next(response.message);
     });
   }
 
   setupEchoPrivate(userId: number) {
 
-    if (!this.privateDone) {
-      this.Echo = new Echo({
-        broadcaster: 'reverb',
-        key: 'ixyw7gpei8mjty0vi0n5',
-        wsHost: 'meetoplay.duckdns.org',
-        wsPort: 8085,
-        wssPort: 443,
-        forceTLS: false,
-        enabledTransports: ['ws', 'wss'],
-        auth: {
-          headers: {
-            'Authorization': 'Bearer ' + this.token,
-          },
+    if (this.privateDone) return;
+
+    this.Echo = new Echo({
+      broadcaster: 'reverb',
+      key: 'ixyw7gpei8mjty0vi0n5',
+      wsHost: 'meetoplay.duckdns.org',
+      wsPort: 8085,
+      wssPort: 443,
+      forceTLS: true,
+      enabledTransports: ['ws', 'wss'],
+      auth: {
+        headers: {
+          'Authorization': 'Bearer ' + this.token,
         },
-        authEndpoint: `https://meetoplay.duckdns.org/api/broadcasting/auth`
-      });
+      },
+      authEndpoint: `https://meetoplay.duckdns.org/api/broadcasting/auth`
+    });
 
+    this.privateMessagesChannel = `App.Models.User.${userId}`;
 
-      this.privateMessagesChannel = `App.Models.User.${userId}`;
+    this.Echo.private(this.privateMessagesChannel).listen('GotMessage', (response: any) => {
+      console.log("From private channel");
+      console.log(response);
 
-      this.Echo.private(this.privateMessagesChannel).listen('GotMessage', (response: any) => {
-        console.log("From private channel");
-        console.log(response);
-        this.privateMessageSource.next(response.message);
-      });
+      this.privateMessageSource.next(response.message);
+    });
 
-      this.privateDone = true;
-    }
+    this.privateDone = true;
+
   }
 
 }
